6c62ef7ac8409aac292754b89e768158d1ec0818
sasha v <volfym79@gmail.com>
2023-06-22 22:28:54 +0200

    continue fe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -101,6 +101,62 @@ const Home: NextPage = () => {
       desc: "peel from citrus fruit, provides a bright and warm acidity",
       alt: "orange, lemon, lime, citron, Citrica Ceruluea",
     },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
+    {
+      name: "Citrus Peel",
+      amount: 1,
+      unit: "g",
+      desc: "peel from citrus fruit, provides a bright and warm acidity",
+      alt: "orange, lemon, lime, citron, Citrica Ceruluea",
+    },
   ];
   const steps = ["Mix ingredients"];
 
@@ -114,45 +170,45 @@ const Home: NextPage = () => {
 
       <div className="flex h-screen w-screen items-center justify-center">
         <div className="flex h-full flex-col items-center justify-between 2xl:max-w-7xl">
-          <header className="flex h-28 w-full flex-row items-center justify-between gap-4 p-4 text-lg">
+          <header className="flex h-28 w-full flex-row items-center justify-between gap-4 p-4 text-3xl">
             <div>
               {user.isSignedIn && <UserButton afterSignOutUrl="/" />}
               {!user.isSignedIn && <SignInButton />}
             </div>
 
-            <div className="flex flex-row justify-between gap-12 text-2xl">
+            <div className="flex flex-row justify-between gap-12">
               <div>Create</div>
               <div>View</div>
             </div>
           </header>
 
-          <main className="flex w-10/12 flex-grow flex-col gap-3 pt-12">
-            <div className="flex justify-between text-xl">
+          <main className="flex w-10/12 flex-grow flex-col gap-3 py-4">
+            <div className="flex justify-between text-2xl">
               <div className="font-medium">{amaroName}</div>
               <div>
                 {defaultAmount} {defaultMeasure}
               </div>
             </div>
 
-            <div className="h-1 w-full rounded-md bg-orange-600"></div>
+            <div className="mb-4 h-1 w-full rounded-md bg-orange-600"></div>
 
-            <div className="grid grid-flow-col grid-cols-[8fr_8fr_3fr] grid-rows-[1fr_3fr_3fr] gap-x-10">
-              <div className="text-center italic text-orange-800">
+            <div className="grid grid-flow-col grid-cols-[6fr_96px_6fr_24px_3fr] grid-rows-[1fr_24px_3fr_24px_2fr]">
+              <div className="pr-16 text-left italic text-orange-800">
                 {tastingNotes}
               </div>
 
-              <div className="col-start-2 flex flex-row flex-wrap justify-center gap-x-8 gap-y-2">
+              <div className="col-start-3 flex flex-row flex-wrap justify-center gap-x-8 gap-y-2">
                 {tags.map((tag) => (
                   <div
                     key={tag}
-                    className="h-fit w-fit rounded-md bg-orange-300 px-2 py-1"
+                    className="h-fit w-fit rounded-md bg-orange-300 px-4 py-1"
                   >
                     {tag}
                   </div>
                 ))}
               </div>
 
-              <div className="col-start-3 flex flex-col items-center">
+              <div className="col-start-5 flex flex-col items-center">
                 <div className="flex flex-row gap-12">
                   <div className="flex flex-col">
                     <div className="text-xs">Uploaded by</div>
@@ -164,9 +220,9 @@ const Home: NextPage = () => {
                 <div className="text-xs">{date}</div>
               </div>
 
-              <div className="row-span-6 row-start-2 aspect-[7/8] w-full rounded-3xl bg-black"></div>
+              <div className="row-span-3 row-start-3 aspect-[7/8] max-w-sm rounded-3xl bg-black"></div>
 
-              <div className="grid grid-flow-dense auto-rows-min  grid-cols-2">
+              <div className="col-span-4 col-start-3 row-span-1 row-start-3 grid grid-flow-dense auto-rows-min grid-cols-2 self-center text-lg">
                 {ingredients.map((i) => (
                   <div key={i.name} className="">
                     {i.amount}
@@ -175,7 +231,7 @@ const Home: NextPage = () => {
                 ))}
               </div>
 
-              <div>
+              <div className="col-span-4 col-start-3 row-span-1 row-start-5 text-lg">
                 <ol className="list-decimal">
                   {steps.map((s) => (
                     <li key={s}>{s}</li>
@@ -186,26 +242,26 @@ const Home: NextPage = () => {
           </main>
 
           <section className="flex h-2/5 w-10/12 flex-col justify-evenly  text-center">
-            <div>SIMILAR RECIPES</div>
+            <div className="text-2xl">Similar Recipes</div>
             <div className="flex w-full items-center justify-between">
               <div className="flex flex-col gap-6 py-6">
-                <div className="h-72 bg-black rounded-3xl aspect-[7/8]"></div>
+                <div className="aspect-[7/8] h-72 rounded-3xl bg-black"></div>
                 <div>Testing 1</div>
               </div>
 
               <div className="flex flex-col gap-6 py-6">
-                <div className="h-72 bg-black rounded-3xl aspect-[7/8]"></div>
+                <div className="aspect-[7/8] h-72 rounded-3xl bg-black"></div>
                 <div>Testing 1</div>
               </div>
 
               <div className="flex flex-col gap-6 py-6">
-                <div className="h-72 bg-black rounded-3xl aspect-[7/8]"></div>
+                <div className="aspect-[7/8] h-72 rounded-3xl bg-black"></div>
                 <div>Testing 1</div>
               </div>
             </div>
           </section>
 
-          <footer className="flex justify-center pb-4">
+          <footer className="flex justify-center pb-4 text-sm">
             © Andre Sammut <br />
             sammut.dev [gh] [li]
           </footer>
